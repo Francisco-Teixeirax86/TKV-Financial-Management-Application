1199f1710d3db78a2303ccab7a13eef4d2dcfa12
Francisco-Teixeirax86 <franjateixeira@gmail.com>
2024-09-05 18:01:57 +0100

    Implemented more basic handlers
    Implemented more basic handlers:
    - Implemented deposit, withdraw and transfer handlers. Tested with postman
    
    TESTS: OK

--- backend/data/store.go
+++ backend/data/store.go
@@ -54,3 +54,69 @@ func (s *Store) UpdateBalance(accountID string, balance float64) error {
 	s.accounts[accountID] = balance
 	return nil
 }
+
+// Deposit updated the balance of accountID by depositing a new amount
+func (s *Store) Deposit(accountID string, amount float64) error {
+	if amount <= 0 {
+		return errors.New("deposit amount must be greater than zero")
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, exists := s.accounts[accountID]; !exists {
+		return errors.New("account does not exist")
+	}
+
+	s.accounts[accountID] += amount
+	return nil
+}
+
+// Withdraw update the balance of accountID by withdrawing funds
+func (s *Store) Withdraw(accountID string, amount float64) error {
+	if amount <= 0 {
+		return errors.New("withdraw amount must be greater than zero")
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	balance, exists := s.accounts[accountID]
+	if !exists {
+		return errors.New("account does not exist")
+	}
+
+	if balance < amount {
+		return errors.New("insufficient funds")
+	}
+
+	return nil
+}
+
+func (s *Store) Transfer(fromAccountID, toAccountID string, amount float64) error {
+	if amount <= 0 {
+		return errors.New("transfer amount must be greater than zero")
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	fromBalance, fromExists := s.accounts[fromAccountID]
+	toBalance, toExists := s.accounts[toAccountID]
+
+	if !fromExists {
+		return errors.New("source account does not exist")
+	}
+
+	if !toExists {
+		return errors.New("destination account does not exist")
+	}
+
+	if fromBalance < amount {
+		return errors.New("insufficient funds in the source account")
+	}
+
+	s.accounts[toAccountID] = toBalance + amount
+	s.accounts[fromAccountID] = fromBalance - amount
+
+	return nil
+}
--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"backend/data"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 var store *data.Store
@@ -19,6 +20,8 @@ func main() {
 
 	http.HandleFunc("/balance", getBalanceHandler)
 	http.HandleFunc("/deposit", depositHandler)
+	http.HandleFunc("/withdraw", withdrawHandler)
+	http.HandleFunc("/transfer", transferHandler)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to the TKV Financial App!")
@@ -33,25 +36,33 @@ func main() {
 
 }
 
+// depositHandler Handler for the deposit request
 func depositHandler(writer http.ResponseWriter, request *http.Request) {
 	accountID := request.URL.Query().Get("accountID")
-	amount := request.URL.Query().Get("amount")
-	if amount == "" || accountID == "" {
+	amountStr := request.URL.Query().Get("amount")
+	if amountStr == "" || accountID == "" {
 		http.Error(writer, "AccountID and Amount are required to update a balance", http.StatusBadRequest)
 		return
 	}
 
-	depositAmount := 100.0 //FOR TESTING  ONLY
-	err := store.UpdateBalance(accountID, depositAmount)
+	amount, err := strconv.ParseFloat(amountStr, 64)
+
 	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		http.Error(writer, "invalid amount", http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Fprintf(writer, "Account %s has been successfully updated with amoun %s", accountID, depositAmount)
+	err = store.Deposit(accountID, amount)
+	if err != nil {
+		http.Error(writer, "failed to update balance", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(writer, "Account %s has been successfully updated with amoun %s", accountID, amount)
 
 }
 
+// getBalanceHandler Handler for the get balance request
 func getBalanceHandler(writer http.ResponseWriter, request *http.Request) {
 	accountID := request.URL.Query().Get("accountID")
 	if accountID == "" {
@@ -65,5 +76,52 @@ func getBalanceHandler(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(writer, "Account %s, Balance %.2f\n", accountID, balance)
+	fmt.Fprintf(writer, "Account %s, Balance %.2f \n", accountID, balance)
+}
+
+func withdrawHandler(writer http.ResponseWriter, request *http.Request) {
+	accountID := request.URL.Query().Get("accountID")
+	amountStr := request.URL.Query().Get("amount")
+
+	if amountStr == "" || accountID == "" {
+		http.Error(writer, "AccountID and Amount are required to withdraw a balance", http.StatusBadRequest)
+		return
+	}
+
+	amount, err := strconv.ParseFloat(amountStr, 64)
+	if err != nil {
+		http.Error(writer, "invalid amount", http.StatusInternalServerError)
+	}
+
+	err = store.Withdraw(accountID, amount)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(writer, "Withrew %.2f from Account: %s \n", amount, accountID)
+}
+
+func transferHandler(writer http.ResponseWriter, request *http.Request) {
+	fromAccountID := request.URL.Query().Get("fromAccountID")
+	toAccountID := request.URL.Query().Get("toAccountID")
+	amountStr := request.URL.Query().Get("amount")
+
+	if amountStr == "" || fromAccountID == "" || toAccountID == "" {
+		http.Error(writer, "fromAccountID, toAccountID and amount are required to transfer", http.StatusBadRequest)
+	}
+
+	amount, err := strconv.ParseFloat(amountStr, 64)
+	if err != nil {
+		http.Error(writer, "invalid amount", http.StatusBadRequest)
+	}
+
+	err = store.Transfer(fromAccountID, toAccountID, amount)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(writer, "Transfered %.2f from Account: %s to Account: %s\n", amount, fromAccountID, toAccountID)
+
 }
